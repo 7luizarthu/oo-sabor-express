c628b432f6fe01fb8ed883d317972e31b6ebbb2c
7luizarthu <contatoarthur088@gmail.com>
2026-09-19 15:06:01 -0300

    Criando as classes do cardapio

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from modelos.restaurante import Restaurante
-
-
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_japones = Restaurante('Japa', 'Japonesa')
-restaurante_japones.receber_avaliacao('Gui', 10)
-restaurante_japones.receber_avaliacao('Lais', 8)
-restaurante_japones.receber_avaliacao('Arthur', 5)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
 
+restaurante_japones.adicionar_no_cardapio(bebida_suco)
+restaurante_japones.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_japones.exibir_cardapio
 
 if __name__ == '__main__':
     main()
