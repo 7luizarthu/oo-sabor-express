125ab5c36c95c0a53841bf86842b218727655876
7luizarthu <contatoarthur088@gmail.com>
2026-09-18 21:23:07 -0300

    Criando a class de Avaliação

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
 
+restaurante_mexicano.alternar_estado()
+
 def main():
-    pass
+    Restaurante.listar_restaurantes()
 
 if __name__ == '__main__':
-    main
+    main()
