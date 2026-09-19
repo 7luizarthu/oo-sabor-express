966b3b90c20c766a5c3e2a5459b9f7bd60b83960
7luizarthu <contatoarthur088@gmail.com>
2026-09-18 21:07:46 -0300

    first commit

---
+++ modelos/restaurante.py
@@ -0,0 +1,25 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self._nome} | {self.categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'categoria'.ljust(25)} | {'status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} - {restaurante._ativo}')
+
+    @property
+    def ativo(self):
+        return '✅' if self._ativo else '❌'
+        
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
